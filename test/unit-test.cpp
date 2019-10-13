c17096fd2cf9e42bc2be255ed249c5d3ad7069d3
Kacper Makuch <kacbull007@gmail.com>
2019-10-13 22:48:02 +0200

    Add unit tests

--- test/unit-test.cpp
+++ test/unit-test.cpp
@@ -4,7 +4,16 @@
 // for each SECTION the TEST_CASE is executed from the start
 
 TEST_CASE("Single Unit on the battlefield") {
-    SECTION("") {
-        REQUIRE();
+    Graphics* graphics;
+    Unit unit(1, 1, 0, graphics);
+
+    SECTION("Test if unit is correctly initialized") {
+        REQUIRE(unit.position->x == 1 && unit.position->y == 1);
+    }
+
+    SECTION("Test if unit is not moving after action()") {
+        int initialPosition = unit.position();
+        unit.action();
+        REQUIRE(unit.position == initialPosition);
     }
 }
