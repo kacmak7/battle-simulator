4f461573459a88372a4c25674192f36013237d02
Kacper Makuch <kacbull007@gmail.com>
2019-12-15 01:32:27 +0100

    add requirements to test_case

--- test/utils-test.cpp
+++ test/utils-test.cpp
@@ -11,7 +11,10 @@ TEST_CASE("Distances between points") {
     Vector2 p3(5,82);
     Vector2 p4(124,821);
     Vector2 p5(4212,8532);
+    Vector2 p6(11544,24512);
 
     REQUIRE(p1.getDistanceToPoint(p2) == (float)47.85);
     REQUIRE(p4.getDistanceToPoint(p2) == (float)784.79);
+    REQUIRE(p3.getDistanceToPoint(p5) == (float)9439.35);
+    REQUIRE(p5.getDistanceToPoint(p6) == (float)17581.77);
 }
