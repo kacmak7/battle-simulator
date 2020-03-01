45e85ccf809c471dc7a11b29409d66765aa7fda4
Kacper Makuch <kacbull007@gmail.com>
2020-03-01 16:08:58 +0100

    start messing with controller

--- src/controller.cpp
+++ src/controller.cpp
@@ -4,21 +4,7 @@
 Controller::Controller() {}
 Controller::~Controller() {}
 
-void Controller::deployUnits(int numberOfUnits, Vector2 pos, int type) {
-    switch (type) {
-        case 1: deployOptimally(numberOfUnits, pos);
-            break;
-        case 2: deployRandomly(numberOfUnits, pos);
-            break;
-        default: deployOptimally(numberOfUnits, pos);
-            break;
+void Controller::deployUnits(int numberOfUnits, Vector2 pos) {
+    for (int i = 0; i < numberOfUnits; i++) {
     }
-}
-
-void Controller::deployOptimally(int numberOfUnits, Vector2 pos) {
-
-}
-
-void Controller::deployRandomly(int numberOfUnits, Vector2 pos) {
-
 }
--- src/controller.h
+++ src/controller.h
@@ -3,22 +3,20 @@
 
 #include "utils.h"
 
+/*
+ * Purpose of this class is to control the gameplay, somewhat like a menu, control panel
+ */
+
 class Controller {
 public:
     Controller();
     ~Controller();
 
     //TODO: make a method generating units on the field near given position
-    //TODO: implement logic of units deployment (exact initial place of every unit assuming they're circles)
 
-    // Generates and deploys units to battle field
-    void deployUnits(int numberOfUnits, Vector2 pos, int type);
+    // Generates and deploys units to the battlefield
+    void deployUnits(int numberOfUnits, Vector2 pos);
 
-private:
-    // Algorithms of units deployment
-    void deployOptimally(int numberOfUnits, Vector2 pos);
-    void deployRandomly(int numberOfUnits, Vector2 pos);
 };
-//TODO: pointers ;)
 
 #endif
